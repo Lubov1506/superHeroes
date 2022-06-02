2c345e57c6c06dc985ab4a29e62a4e318239c289
Lubov <lubaparfyonova@gmail.com>
2022-06-02 19:08:41 +0300

    migration SuperPowers

---
+++ models/superpower.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SuperPower extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  SuperPowers.init({
+    superPower: {
+      field: 'super_power',
+      allowNull: false,
+      type: DataTypes.STRING}
+  }, {
+    sequelize,
+    modelName: 'SuperPower',
+    tableName: 'superpowers',
+    underscored: true
+  });
+  return SuperPower;
+};
