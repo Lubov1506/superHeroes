49662a391304d2ddfe50907dcfdb7729e2d605a2
Lubov <lubaparfyonova@gmail.com>
2022-06-03 19:20:00 +0300

    Add associates belongsToMany

--- models/superhero.js
+++ models/superhero.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      SuperHero.belongsToMany(models.SuperPower, {
+        through: 'heroes_to_powers',
+        foreignKey: 'superPowerId'
+      })
     }
   }
   SuperHero.init({
--- models/superpower.js
+++ models/superpower.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      SuperPower.belongsToMany(models.SuperHero, {
+        through: 'heroes_to_powers',
+        foreignKey: 'superHeroId'
+      })
     }
   }
   SuperPower.init({
